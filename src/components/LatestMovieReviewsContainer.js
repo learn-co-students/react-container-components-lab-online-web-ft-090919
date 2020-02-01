8e8b8e272630717930c5af770ec55b7195d8bc46
Jendnice <Pipeline38@hotmail.com>
2020-02-01 18:12:25 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,46 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
+// full URL with key above: 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ`     
+
+
 // Code LatestMovieReviewsContainer Here
+
+
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(reviewsData => this.setState({ reviews: reviewsData.results })
+        )
+    }
+
+    // addReviews = (reviewsData) => {
+    //     // see what reviews Data returns here to format correctly
+
+    //     this.setState({
+    //         reviews: reviewsData.results
+    //     })
+    // }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+
+}
+
+
+
+
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,68 @@
 // Code MovieReviews Here
+
+import React from 'react'
+
+
+// title: reviewsData.results[0].display_title
+// short summary: reviewsData.result[0].short_summary
+
+// below "reviews" should equate to reviewsData.results
+
+const MovieReviews = ({ reviews }) => {
+
+    return (
+        <div className="review-list">
+            { reviews.map(({ headline, summary_short, display_title }, index ) => 
+            <div key={index} className="review" >
+                <h3>
+                    { headline }
+                </h3>
+                <p>
+                    { display_title } - { summary_short }
+                </p>
+            </div>
+            )}
+        </div>
+    )
+}
+
+
+
+
+
+export default MovieReviews
+
+
+
+
+
+// Ex. of one review object:
+
+// this.state.reviews[0]
+
+// {display_title: "Taylor Swift: Miss Americana", mpaa_rating: "", critics_pick: 1, byline: "Wesley Morris", headline: "‘Taylor Swift: Miss Americana’ Review: A Star, Scathingly Alone", …}
+// display_title: "Taylor Swift: Miss Americana"
+// mpaa_rating: ""
+// critics_pick: 1
+// byline: "Wesley Morris"
+// headline: "‘Taylor Swift: Miss Americana’ Review: A Star, Scathingly Alone"
+// summary_short: "Self-critical, grown up and ready, perhaps, to deliver a message beyond the music."
+// publication_date: "2020-01-30"
+// opening_date: "2020-01-31"
+// date_updated: "2020-02-01 17:44:17"
+// link:
+// type: "article"
+// url: "http://www.nytimes.com/2020/01/30/movies/taylor-swift-miss-americana-review.html"
+// suggested_link_text: "Read the New York Times Review of Taylor Swift: Miss Americana"
+// __proto__: Object
+// multimedia:
+// type: "mediumThreeByTwo210"
+// src: "https://static01.nyt.com/images/2020/01/29/arts/29TAYLOR/29TAYLOR-mediumThreeByTwo210.jpg"
+// width: 210
+// height: 140
+// __proto__: Object
+// __proto__: Object
+
+
+
+
